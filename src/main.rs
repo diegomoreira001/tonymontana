5a6d7fd4f920476dd04ae6fa637f340fe96398d8
dmoreira <diegomoreira00@gmail.com>
2019-01-27 17:24:56 -0300

    - TCP Packet Handling - IPv4 Handling - IPv6 Handling - Basic Display - NEXT: pretty-print packets - BUG: some packets cannot match the protocol even though they are known IPv4

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use std::mem;
-use std::net;
-use std::process;
-use std::env;
+extern crate pnet;
+
+use pnet::datalink::{self, NetworkInterface};
+
 use pnet::packet::ethernet::{EtherTypes, EthernetPacket, MutableEthernetPacket};
+use pnet::packet::ip::{IpNextHeaderProtocol, IpNextHeaderProtocols};
 use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::ipv6::Ipv6Packet;
 use pnet::packet::tcp::TcpPacket;
-use pnet::datalink::{self, NetworkInterface};
 use pnet::packet::Packet;
 use pnet::util::MacAddr;
 
-use pnet::transport;
+use std::env;
+use std::io::{self, Write};
+use std::net::IpAddr;
+use std::process;
 
 //Simple light-weight packet sniffer
 //We'll trust in libpnet. Should be cross-platform.
@@ -27,16 +30,19 @@ fn main() {
 
 
     //1. Check args and show usage. Something like -> USAGE: tony eth0
-    let first_arg = env::args().nth(1);
-    if first_arg.is_none() {
-        eprintln!("USAGE: tony <IFACE>");
-        process::exit(1);
-    }
+    let first_arg = match env::args().nth(1) {
+        Some(n) => n,
+        None => {
+            writeln!(io::stderr(), "USAGE: tony <IFACE>").unwrap();
+            process::exit(1);
+        }
+    };
 
+    let predicate = |net_iface: &NetworkInterface| net_iface.name == first_arg;
     //2. Retrieve interface from args, search for it at OS level, and allocate it.
     let network_interfaces = datalink::interfaces();
     let network_interface = network_interfaces.into_iter()
-        .filter(|net_iface: &NetworkInterface| net_iface.name == first_arg)
+        .filter(predicate)
         .next()
         .unwrap();
 
@@ -105,7 +111,93 @@ fn main() {
 
 fn handle_ethernet_frame(network_interface: &NetworkInterface, ethernet_packet: &EthernetPacket) {
 
+    let interface_name = &network_interface.name[..];
     //check packet type, may be IPv4, IPv6
     //generic arp types won't panic but will be discarded, no warning either
+    match ethernet_packet.get_ethertype() {
+        EtherTypes::Ipv4 => {
+            let header = Ipv4Packet::new(ethernet_packet.payload());
+            if let Some(header) = header {
+                handle_transport_protocol(
+                    interface_name,
+                    IpAddr::V4(header.get_source()),
+                    IpAddr::V4(header.get_destination()),
+                    header.get_next_level_protocol(),
+                    header.payload(),
+                );
+            } else {
+                println!("[{}]: Malformed IPv4 Packet", interface_name);
+            }
+        }
+        EtherTypes::Ipv6 => {
+            let header = Ipv6Packet::new(ethernet_packet.payload());
+            if let Some(header) = header {
+                handle_transport_protocol(
+                    interface_name,
+                    IpAddr::V6(header.get_source()),
+                    IpAddr::V6(header.get_destination()),
+                    header.get_next_header(),
+                    header.payload(),
+                );
+            } else {
+                println!("[{}]: Malformed IPv6 Packet", interface_name);
+            }
+        }
+        _ => println!(
+            "[{}]: Unknown packet: {} > {}; ethertype: {:?} length: {}",
+            interface_name,
+            ethernet_packet.get_source(),
+            ethernet_packet.get_destination(),
+            ethernet_packet.get_ethertype(),
+            ethernet_packet.packet().len()
+        ),
+    }
 
+}
+
+fn handle_transport_protocol(
+    interface_name: &str,
+    source: IpAddr,
+    destination: IpAddr,
+    protocol: IpNextHeaderProtocol,
+    packet: &[u8],
+) {
+    match protocol {
+         IpNextHeaderProtocols::Tcp => {
+            handle_tcp_packet(interface_name, source, destination, packet)
+         }
+         _ => println!(
+            "[{}]: Unknown {} packet: {} > {}; protocol: {:?} length: {}",
+            interface_name,
+            match source {
+                IpAddr::V4(..) => "IPv4",
+                _ => "IPv6",
+            },
+            source,
+            destination,
+            protocol,
+            packet.len()
+
+        ),
+    }
+}
+
+
+//TODO: format packet
+fn handle_tcp_packet(interface_name: &str, source: IpAddr, destination: IpAddr, packet: &[u8]) {
+    let tcp = TcpPacket::new(packet);
+    if let Some(tcp) = tcp {
+        println!(
+            "[{}]: TCP Packet: {}:{} > {}:{}; length: {}; packet: {:?}",
+            interface_name,
+            source,
+            tcp.get_source(),
+            destination,
+            tcp.get_destination(),
+            packet.len(),
+            packet
+        );
+    } else {
+        println!("[{}]: Malformed TCP Packet", interface_name);
+    }
 }
